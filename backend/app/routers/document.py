c6c10a000d6ce01253c84207d84c330e2d421aaf
dw <hudawei@collectbits.net>
2026-04-20 10:29:50 +0800

    html表格自适应测试7

--- backend/app/routers/document.py
+++ backend/app/routers/document.py
@@ -277,6 +277,7 @@ async def update_html_by_node_new(
 
         # 调用表格宽度适配接口
         # new_file_path = call_set_table_width(temp_docx_path_1)
+
         new_file_path = temp_docx_path_1
         with open(new_file_path, "rb") as _f:
             file_bytes = _f.read()
